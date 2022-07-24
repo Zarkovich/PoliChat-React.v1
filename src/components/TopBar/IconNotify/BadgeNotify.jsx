9d2dfa66e93291ab883621bd4455979d08c27dcb
Gustavo Vieira <gustavovieirazu@gmail.com>
2022-07-24 11:39:12 -0300

    Animation

--- src/components/TopBar/IconNotify/BadgeNotify.jsx
+++ src/components/TopBar/IconNotify/BadgeNotify.jsx
@@ -1,17 +1,15 @@
-import Badge from "react-bootstrap/Badge";
 import { GoAlert } from "react-icons/go";
-
 import "../IconNotify/BadgeNotify.scss";
 
 const BadgeNotify = ({ typeBadge, numberCounter }) => {
   if (typeBadge === "counter") {
     return (
-      <Badge pill bg='secondary' className='notify__style'>
-        {numberCounter}
-      </Badge>
+      <div className='Notification'>
+      <div>{numberCounter}</div>
+    </div>
     );
   } else if (typeBadge === "alert") {
-    return <GoAlert className='notify__style notify__style--alert' />;
+    return <GoAlert className='Alert' />;
   }
 };
 
